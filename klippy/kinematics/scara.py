9d3126d1c81c9d6bba1bd8759127b5f3dde78618
humidifier8892 <109432965+humidifier8892@users.noreply.github.com>
2022-12-06 23:20:10 -0500

    try to fix getfloat call

--- klippy/kinematics/scara.py
+++ klippy/kinematics/scara.py
@@ -13,16 +13,16 @@ class ScaraKinematics:
         logging.info("test set: %s", a)
         rail_proximal = stepper.PrinterRail(config.getsection('stepper_proximal'),
                                              units_in_radians=True)
-        self.proximal_length = rail_proximal.getfloat(rail_proximal, axis_len)
+        self.proximal_length = rail_proximal.getfloat(rail_proximal, 'position_max')
         
         rail_distal = stepper.PrinterRail(config.getsection('stepper_distal'),
                                              units_in_radians=True)
-        self.distal_length = rail_distal.getfloat(rail_distal, axis_len)
+        self.distal_length = rail_distal.getfloat(rail_distal, 'position_max')
 
         rail_proximal.setup_itersolve(
             'scara_stepper_alloc', 'p',
             self.proximal_length, self.distal_length, self.crosstalk, self.arm_mode)
-            
+
         rail_distal.setup_itersolve(
             'scara_stepper_alloc', 'd',
             self.proximal_length, self.distal_length, self.crosstalk, self.arm_mode)
